6d65c4845f01b4e5723eeb566b0f75942b3e9d49
sajieun <sajieun000721@naver.com>
2025-01-14 14:33:54 +0900

    Query Method란

--- src/main/java/org/sparta/memo/repository/MemoRepository.java
+++ src/main/java/org/sparta/memo/repository/MemoRepository.java
@@ -3,9 +3,10 @@ package org.sparta.memo.repository;
 import org.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
 
 }
